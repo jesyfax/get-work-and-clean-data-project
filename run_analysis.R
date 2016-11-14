c2072628935d808621a12a5ec3dee508ec1f7da0
Jesin Fahad <jesyfax@gmail.com>
2016-11-14 18:14:38 +0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,20 @@
-# Download the file and put the file in the data folder
+
+#Section1: Merge the training and the test sets to create one data set.
+##################################################################################
+## Download the file and put the file in the data folder
 
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="./data/Dataset.zip")
-###############################################################################
-# Unzip the file and get the list of files in the unzipped folder
+
+## Unzip the file and get the list of files in the unzipped folder
 
 unzip(zipfile="./data/Dataset.zip",exdir="./data")
 path_rf <- file.path("./data" , "UCI HAR Dataset")
 files<-list.files(path_rf, recursive=TRUE)
 files
-###############################################################################
-# Read data from the files into the variables
+
+## Read data from the files into the variables
 ## Read the Activity files
 dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
 dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
@@ -24,7 +27,6 @@ dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),h
 dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
 dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
 
-###############################################################################
 ## Look at the properties of the above varibles
 
 str(dataActivityTest)
@@ -33,8 +35,6 @@ str(dataSubjectTrain)
 str(dataSubjectTest)
 str(dataFeaturesTest)
 str(dataFeaturesTrain)
-
-###############################################################################
 
 ## Bind the data tables by rows
 dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
@@ -51,32 +51,38 @@ names(dataFeatures)<- dataFeaturesNames$V2
 dataCombine <- cbind(dataSubject, dataActivity)
 Data <- cbind(dataFeatures, dataCombine)
 
+#Section2: Extract only the measurements on the mean and standard deviation for each measurement.
+##################################################################################
 ##Subset by measurements on the mean and standard deviation
 subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
 
-#Subset the data frame Data by seleted names of Features
+##Subset the data frame Data by seleted names of Features
 selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
 Data<-subset(Data,select=selectedNames)
 
-#Check the structures of the data frame Data
+##Check the structures of the data frame Data
 str(Data)
 
+
+#Section 3. Use descriptive activity names to name the activities in the data set
 ################################################################################
-#Name the activities in the data set
+##Name the activities in the data set
 
 activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
 
+## Section 4. Appropriately label the data set with descriptive activity names.
 names(Data)<-gsub("^t", "time", names(Data))
 names(Data)<-gsub("^f", "frequency", names(Data))
 names(Data)<-gsub("Acc", "Accelerometer", names(Data))
 names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
 names(Data)<-gsub("Mag", "Magnitude", names(Data))
 names(Data)<-gsub("BodyBody", "Body", names(Data))
-#check
+###check
 names(Data)
 
+
+## Section 5. Create a second, independent tidy data set with the average of each variable for each activity and each subject. 
 ##################################################################################
-#second, independent tidy data set is created with the average of each variable for each activity and each subject 
 library(dplyr);
 Data2<-aggregate(. ~subject + activity, Data, mean)
 Data2<-Data2[order(Data2$subject,Data2$activity),]
